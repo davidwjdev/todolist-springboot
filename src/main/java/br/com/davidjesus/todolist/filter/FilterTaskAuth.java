cfd8ff8399836b60658d789f2d174d98fd6bd056
David Wilson de Jesus <80333501+davidwjdev@users.noreply.github.com>
2023-10-13 22:38:50 -0300

    (Feat): Autenticação e validação para criação de task

---
+++ src/main/java/br/com/davidjesus/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,63 @@
+package br.com.davidjesus.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.davidjesus.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository iUserRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        var servletPath = request.getServletPath();
+        if (servletPath.equals("/tasks/")) {
+            // Pega autenticação de usuário
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecoded);
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+            System.out.println(username);
+            System.out.println(password);
+            // valida usuário
+            var user = this.iUserRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(HttpStatus.UNAUTHORIZED.value());
+            } else {
+                // valida senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    // continua o fluxo
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(HttpStatus.UNAUTHORIZED.value());
+
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+
+        }
+
+    }
+
+}
